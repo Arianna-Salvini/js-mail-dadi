2e46e685c4928b92f43ac8ff675390f0b2677d70
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-22 17:54:56 +0100

    Fix email validation and alert message

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,25 +11,30 @@ console.log(userEmail);
 // - Controlla che sia nella lista di chi può accedere
 //      - creare un ARRAY di indirizzi email
 let emailList = ["ari@gmail.com",  "john.doe@example.com", "jane.smith@example.com", "alice123@example.com", "bob.jones@example.com", "emily_22@example.com"];
-let valid = "Hello there! welcome!";
-let invalid = "Sorry, your e-mail could not be find";
-console.log(emailList, valid, invalid);
+let valid = false;
+
+console.log(emailList, valid);
 
  // - creare un CICLO per il confronto dell'email con l'array
 for (let i = 0; i < emailList.length; i++) {
     let emailCheck = emailList[i];
-    console.log(emailCheck);
 
     if (userEmail == emailCheck) {
-        console.log (valid)
-        alert(valid)
-    } else if(userEmail != emailCheck) {
-         alert(invalid)
-    } 
+       valid = true
+       console.log(valid);
+    }
 }
 
+if (valid){
+    alert("Hello there! welcome!")
+} else {
+    alert("Sorry, your e-mail could not be find")
+}
 
 
 // - Stampa un messaggio appropriato sull’esito del controllo.
 //      - SE l'indizizzo è nella lista inviare un ALERT con messaggio A
 //      - ALTRIMENTI invia ALERT con messaggio B
+
+
+
