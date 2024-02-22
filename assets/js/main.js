f6c6acae8e29c2024036932f4d8a001f752da29f
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-22 18:12:40 +0100

    Add const for players 1 for user and 1 for computer and add an array from 1 to 6 for number of dice

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,39 +2,65 @@ console.log("Do you wanna play? Give me your email.");
 
 ///// Chiedere user email/////
 
-// - Chiedi all’utente la sua email
-//     - PROMP per chiedere email
+// // - Chiedi all’utente la sua email
+// //    - PROMP per chiedere email
 
-const userEmail = prompt("Inserisci il tuo indirizzo E-mail");
-console.log(userEmail);
+// const userEmail = prompt("Inserisci il tuo indirizzo E-mail");
+// console.log(userEmail);
 
-// - Controlla che sia nella lista di chi può accedere
-//      - creare un ARRAY di indirizzi email
-let emailList = ["ari@gmail.com",  "john.doe@example.com", "jane.smith@example.com", "alice123@example.com", "bob.jones@example.com", "emily_22@example.com"];
-let valid = false;
+// // - Controlla che sia nella lista di chi può accedere
+// //      - creare un ARRAY di indirizzi email
+// let emailList = ["ari@gmail.com",  "john.doe@example.com", "jane.smith@example.com", "alice123@example.com", "bob.jones@example.com", "emily_22@example.com"];
+// let valid = false;
 
-console.log(emailList, valid);
+// console.log(emailList, valid);
 
- // - creare un CICLO per il confronto dell'email con l'array
-for (let i = 0; i < emailList.length; i++) {
-    let emailCheck = emailList[i];
+//  // - creare un CICLO per il confronto dell'email con l'array
+// for (let i = 0; i < emailList.length; i++) {
+//     let emailCheck = emailList[i];
 
-    if (userEmail == emailCheck) {
-       valid = true
-       console.log(valid);
-    }
-}
+//     if (userEmail == emailCheck) {
+//        valid = true
+//        console.log(valid);
+//     }
+// }
 
-if (valid){
-    alert("Hello there! welcome!")
-} else {
-    alert("Sorry, your e-mail could not be find")
-}
+// // - Stampa un messaggio appropriato sull’esito del controllo.
+// //      - SE l'indizizzo è nella lista inviare un ALERT con messaggio A
+// //      - ALTRIMENTI invia ALERT con messaggio B
+
+// if (valid){
+//     alert("Hello there! welcome!")
+// } else {
+//     alert("Sorry, your e-mail could not be find")
+// }
 
 
-// - Stampa un messaggio appropriato sull’esito del controllo.
-//      - SE l'indizizzo è nella lista inviare un ALERT con messaggio A
-//      - ALTRIMENTI invia ALERT con messaggio B
+/////Gioco dei dadi/////
+// - Creare variabili per 2 giocatori
+//      - variabile 1 -> prompt = 1inserisci il tuo nickname
+//      - variabile 2 -> computer
+
+const computerGamer = "Your computer"
+const userGamer = prompt("Inserisci il tuo nickname")
+
+console.log(computerGamer);
+console.log(userGamer);
+
+// - Generare un numero random da 1 a 6
+//     - creare un arrai di numeri da 1 a 6
+//     - generazione casuale di numeri Math.random da 1 a 6
+
+  const dice = [1, 2, 3, 4, 5, 6]
+  console.log(dice);
+
+
+// - Stabilire il vincitore, in base a chi fa il punteggio più alto.
+//     - confrontare i numeri generati
+//     - confermare il vincitore che ha il numero maggiore
+
+
+
 
 
 
